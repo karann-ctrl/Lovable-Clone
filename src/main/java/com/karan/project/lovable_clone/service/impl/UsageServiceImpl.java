aa133715591746cbdbc49326f87427d511e732f4
Karan Shivhare <karanshivhare248@gmail.com>
2026-06-21 03:36:13 +0530

    Day 5 - MapStruct and Project Service added

---
+++ src/main/java/com/karan/project/lovable_clone/service/impl/UsageServiceImpl.java
@@ -0,0 +1,20 @@
+package com.karan.project.lovable_clone.service.impl;
+
+import com.karan.project.lovable_clone.dto.subscription.PlanLimitsResponse;
+import com.karan.project.lovable_clone.dto.subscription.UsageTodayResponse;
+import com.karan.project.lovable_clone.service.UsageService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsageServiceImpl implements UsageService {
+
+    @Override
+    public UsageTodayResponse getTodayUsageOfUser(Long userId) {
+        return null;
+    }
+
+    @Override
+    public PlanLimitsResponse getCurrentSubscriptionLimitsOfUser(Long userId) {
+        return null;
+    }
+}
